bfab76d134a0228607c79d366933e5d83526a9b7
ilaijnehc <41381402+ilaijnehc@users.noreply.github.com>
2020-08-27 13:43:22 -0400

    Update benchfinder.py

--- benchfinder.py
+++ benchfinder.py
@@ -1,40 +1,33 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-import requests 
+
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app.config['SECRET_KEY'] = 'whatthefrick'
 
 
-# class ReusableForm(Form): 
-#     name = TextField('Name:', validators=[validators.required()])
-    
-#     def __init__(self, Form):
-#         self.Form = Form
-
-#     @app.route("/", methods=['GET', 'POST'])
-#     def hello():
-#         form = ReusableForm(requests.form)
-    
-#         print(form.errors)
-#         if requests.method == 'POST':
-#             name=requests.form['name']
-#             print(name)
-    
-#         if form.validate():
-#             # Save the comment here.
-#             flash('Hello ' + name)
-#         else:
-#             flash('All the form fields are required. ')
-    
-#         return render_template('hello.html', form=form)
-
-#     hello()
+class ReusableForm(FlaskForm):
+    username = StringField('Username')
+    submit = SubmitField('Submit')
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/send", methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        persons = request.form['persons']
+
+        return render_template("persons.html", persons=persons) 
+    return render_template("index.html")
+
+@app.route("/testsignup")
+def signup():
+    form= ReusableForm()
+    return render_template("testsignup.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
